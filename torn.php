bc07585a9e4f06cc7be66cc82ed66fb3cab38941
tuihblsr <139407884+tuihblsr@users.noreply.github.com>
2023-12-03 19:35:38 -0500

    Delete torn.php

--- torn.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $jsonurl = "http://api.torn.com/user/3062354?selections=networth&key=kEfEFncWpA4zptaj";
-    $json = file_get_contents($jsonurl); //gets output of API
-        
-    $decodedString = json_decode($json, true); //parses API JSON output
-    $nwtotal = $decodedString["networth"]; //Pulls networth out of JSON array
-    echo "Stock Market: ".$nwtotal["stockmarket"]."</br>"; // Spits out networth from stocks
-    echo "Networth Total: ".$nwtotal["total"]."</br>"; // Spits out total networth
-?>
