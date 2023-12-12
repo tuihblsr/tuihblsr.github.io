46c3bf937ae71dcf22430a7fb73f831e57309412
v_tyan <139407884+tuihblsr@users.noreply.github.com>
2023-12-12 15:05:34 +0000

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,14 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-console.log(canvas);
-console.log(ctx);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    ctx.fillStyle = "white";
+    ctx.fillRect(10, 10, 50, 150);
+})
 //
 //
 ctx.fillStyle = "white";
