338f2a6a4f8768876cfddddcc24359f17695190b
tuihblsr <139407884+tuihblsr@users.noreply.github.com>
2024-03-14 12:30:36 -0400

    Update mod.js

--- mod.js
+++ mod.js
@@ -5,7 +5,7 @@ Game.registerMod("vtyansMod",{
     });
     Game.registerHook('check',function(){
       if (name=='v_tyan') Game.Win('Imposter')
-    };
+    });
     Game.registerHook('click',function(){Game.Notify(choose(['A shitty click','A good click.','A solid click.','A mediocre click.','An excellent click!']),'',0,0.5);});
   },
   save:function(){
