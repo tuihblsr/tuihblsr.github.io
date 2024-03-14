b14b5f9145b1995f225c92bedc0da17b9b40bdc8
tuihblsr <139407884+tuihblsr@users.noreply.github.com>
2024-03-14 12:32:38 -0400

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,10 +1,10 @@
 Game.registerMod("vtyansMod",{
   init:function(){
     Game.registerHook('create',function(){
-      new Game.Achievement('Impostor','Name yourself <b>v_tyan</b> <q>please do not impersonate officials.</q>',[17,5]),Game.last.pool="shadow",
+      new Game.Achievement('Impostor','Name yourself <b>v_tyan</b> <q>please do not impersonate officials.</q>',[17,5]),Game.last.pool="shadow";
     });
     Game.registerHook('check',function(){
-      if (name=='v_tyan') Game.Win('Imposter')
+      if (name=='v_tyan') Game.Win('Imposter');
     });
     Game.registerHook('click',function(){Game.Notify(choose(['A shitty click','A good click.','A solid click.','A mediocre click.','An excellent click!']),'',0,0.5);});
   },
