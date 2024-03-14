51055b579ae4168d672158fb13e84f24a9a1bc59
tuihblsr <139407884+tuihblsr@users.noreply.github.com>
2024-03-14 12:10:08 -0400

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,3 +1,13 @@
-Game.registerHook('create',[
-  
-])
+Game.registerMod("vtyansMod",{
+  init:function(){
+    Game.registerHook('create',function(){});
+    Game.registerHook('click',function(){Game.Notify(choose(['A good click.','A solid click.','A mediocre click.','An excellent click!']),'',0,0.5);});
+  },
+  save:function(){
+    
+  },
+  load:function(){
+    
+  },
+})
+
