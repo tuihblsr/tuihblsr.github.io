d885e64a32b2f61c835a67dc1a24501ddac17a4f
tuihblsr <vtyan5291@gmail.com>
2024-03-15 15:10:20 -0400

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,14 +1,15 @@
-this.createAchievements=function(){
-	this.achievements = []
-	this.achievements.push(new Game.Achievement("Imposter", "Name yourself <b>v_tyan</b>"+'<q>Please do not impersonate officials.</q>',[0,0]));Game.last.pool='shadow'
-            
-	Game.Achievements['Imposter'].order=100
-            LocalizeUpgradesAndAchievs()
-        }
-        this.checkAchievements=function(){
-            name=Game.bakeryName.toLowerCase();
-            if (name=='v_tyan') Game.Win('Imposter');
-        }
-        if(Game.ready) this.createAchievements()
-        else Game.registerHook("create", this.createAchievements)
-        Game.registerHook("check", this.checkAchievements)
+Game.registerMod("vtyansMod",{});
+    this.createAchievements=function(){
+        this.achievements = []
+        this.achievements.push(new Game.Achievement("Imposter", "Name yourself <b>v_tyan</b>"+'<q>Please do not impersonate officials.</q>',[0,0]));Game.last.pool='shadow'
+                
+        Game.Achievements['Imposter'].order=100
+                LocalizeUpgradesAndAchievs()
+            }
+            this.checkAchievements=function(){
+                name=Game.bakeryName.toLowerCase();
+                if (name=='v_tyan') Game.Win('Imposter');
+            }
+            if(Game.ready) this.createAchievements()
+            else Game.registerHook("create", this.createAchievements)
+            Game.registerHook("check", this.checkAchievements)
