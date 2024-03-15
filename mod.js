2744ef5b35ce3d5debb44d69f21dfca863c8edc8
tuihblsr <139407884+tuihblsr@users.noreply.github.com>
2024-03-15 15:06:23 -0400

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,19 +1,14 @@
-Game.registerMod("vtyansMod",{
-  init:function(){
-    Game.registerHook('create',function(){
-      new Game.Achievement('Imposter',loc("Name yourself <b>v_tyan</b>.",1)+'<q>Please do not impersonate officials.</q>',[17,5]);Game.last.pool='shadow';
-    });
-    Game.registerHook('check',function(){
-			bakename=Game.bakeryName.toLowerCase();
-      if (bakename=='v_tyan') Game.Win('Imposter');
-    });
-    Game.registerHook('click',function(){Game.Notify(choose(['A shitty click','A good click.','A solid click.','A mediocre click.','An excellent click!']),'',0,0.5);});
-  },
-  save:function(){
-    
-  },
-  load:function(){
-    
-  },
-})
-
+this.createAchievements=function(){
+	this.achievements = []
+	this.achievements.push(new Game.Achievement("Imposter", "Name yourself <b>v_tyan</b>"+'<q>Please do not impersonate officials.</q>',[0,0]));Game.last.pool='shadow'
+            
+	Game.Achievements['Imposter'].order=100
+            LocalizeUpgradesAndAchievs()
+        }
+        this.checkAchievements=function(){
+            name=Game.bakeryName.toLowerCase();
+            if (name=='v_tyan') Game.Win('Imposter');
+        }
+        if(Game.ready) this.createAchievements()
+        else Game.registerHook("create", this.createAchievements)
+        Game.registerHook("check", this.checkAchievements)
