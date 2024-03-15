928c50f78bfafec549b8221130105f11724cd571
tuihblsr <vtyan5291@gmail.com>
2024-03-15 09:57:38 -0400

    Update mod.js

--- mod.js
+++ mod.js
@@ -4,7 +4,9 @@ Game.registerMod("vtyansMod",{
       new Game.Achievement('Imposter',loc("Name yourself <b>v_tyan</b>.",1)+'<q>Please do not impersonate officials.</q>',[17,5]);Game.last.pool='shadow';
     });
     Game.registerHook('check',function(){
-      if (Game.bakeryName=='v_tyan') Game.Win('Imposter');
+      Game.bakeryNameL.textContent=bakename;
+			bakename=Game.bakeryName.toLowerCase();
+      if (bakename=='v_tyan') Game.Win('Imposter');
     });
     Game.registerHook('click',function(){Game.Notify(choose(['A shitty click','A good click.','A solid click.','A mediocre click.','An excellent click!']),'',0,0.5);});
   },
